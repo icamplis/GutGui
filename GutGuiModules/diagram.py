11bc9bfc984e6597d7de073874a6b06a9d645b8e
UGYW <uma.w@outlook.com>
2019-06-13 16:54:37 +0200

    Added framework

--- GutGuiModules/diagram.py
+++ GutGuiModules/diagram.py
@@ -1,3 +1,26 @@
 class Diagram:
     def __init__(self):
+        self.whole_image_label = None
+        self.whole_image_checkbox = None
+        self.whole_image_checkbox_value = None
+
+        self.masked_region_label = None
+        self.masked_region_checkbox = None
+        self.masked_region_checkbox_value = None
+
+    # Helper
+    def _init_widget(self):
+        pass
+
+    def _build_whole_image(self):
+        pass
+
+    def _build_masked_region(self):
+        pass
+
+    # Commands (Callbacks)
+    def __update_whole_image_checked(self):
+        pass
+
+    def __update_masked_region_checked(self):
         pass
