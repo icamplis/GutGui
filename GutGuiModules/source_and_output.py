f4b74d3907657c1e4ea0b61fa5f8053cd249088c
UGYW <uma.w@outlook.com>
2019-06-17 12:44:03 +0200

    Listbox now adds selected data cubes.

--- GutGuiModules/source_and_output.py
+++ GutGuiModules/source_and_output.py
@@ -2,6 +2,7 @@ from GutGuiModules.utility import *
 import numpy as np
 from copy import deepcopy
 from tkinter import filedialog, messagebox
+import os
 
 # Manages the source and output directory
 class SourceAndOutput:
@@ -17,7 +18,8 @@ class SourceAndOutput:
         self.output_dir_label = ""
 
         # Data
-        self.data_cube = None
+        self.data_cubes = []
+        self.data_cube_paths = []
         self.path = ""
         self.data_cube_path_label = None
         self.path_label = None
@@ -25,9 +27,16 @@ class SourceAndOutput:
         # Widget Initialization
         self._init_widgets()
 
-    # Makes a deep copy of the original data cube
-    def get_data_cube(self):
-        return deepcopy(self.data_cube)
+    # Makes a deep copy of the original data cubes
+    def get_data_cubes(self):
+        return deepcopy(self.data_cubes)
+
+    def get_selected_data_cube(self):
+        index = self.selection_listbox.curselection()
+        return self._get_data_cube_by_index(index)
+
+    def _get_data_cube_by_index(self, index):
+        return deepcopy(self.data_cubes[index])
 
     def get_path(self):
         return self.path
@@ -40,28 +49,36 @@ class SourceAndOutput:
 
     def _build_select_dc_button(self):
         self.select_data_cube_button = make_button(self.root, text="Select Data Cube", command=self.__set_data_cube,
-                                                   inner_padx=10, inner_pady=10, outer_padx=(15, 0), outer_pady=0,
+                                                   inner_padx=10, inner_pady=10, outer_padx=(15, 0), outer_pady=5,
                                                    row=1, column=0, width=15)
 
     def _build_select_od_button(self):
         self.select_output_dir_button = make_button(self.root, text="Select Output Folder", command=self.__set_output_dir,
-                                                    inner_padx=10, inner_pady=10, outer_padx=(15, 0), outer_pady=(0, 15),
+                                                    inner_padx=10, inner_pady=10, outer_padx=(15, 0), outer_pady=5,
                                                     row=2, column=0, width=15)
 
     def _build_selection_box(self):
-        self.selection_listbox = make_listbox(self.root, input=None, row=1, column=1)
+        self.selection_listbox = make_listbox(self.root, input=None, row=1, column=1, rowspan=2)
 
+    def _build_delete_button(self):
+        pass
 
     # Commands (Callbacks)
     def __set_data_cube(self):
-        (self.data_cube, dc_path) = self.__process_data_cube()
-        self.data_cube_path_label = make_label(self.root, "Using Data Cube at: " + str(dc_path),
-                                               row=2, column=0, wraplength=160)
+        (data_cube, dc_path) = self.__process_data_cube()
+        concat_dc_path = os.path.basename(os.path.normpath(dc_path))
+        if dc_path in self.data_cube_paths:
+            messagebox.showinfo("Error", "That data cube has already been added.")
+        else:
+            self.data_cube_paths.append(dc_path)
+            self.data_cubes.append(data_cube)
+            self.selection_listbox.insert(END, concat_dc_path)
+            self.selection_listbox.config(width=0)  # resizes to widest path
 
     def __set_output_dir(self):
         self.path = self.__get_path_to_dir("Select a folder for the output to be stored.")
         self.path_label = make_label(self.root, "Using Output Folder at: " + str(self.path),
-                                               row=4, column=0, wraplength=160)
+                                               row=3, column=0, wraplength=160, outer_pady=(2, 5))
 
     def __process_data_cube(self):
         path = self.__get_path_to_file("Select a data cube (ending in .dat)")
@@ -82,3 +99,6 @@ class SourceAndOutput:
     def __get_path_to_dir(self, title):
         path = filedialog.askdirectory(parent=self.root, title=title)
         return path
+
+    def __delete_selected_data_cube(self):
+        pass
