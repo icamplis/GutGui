4df3214365ab2bb5f880a5c9511262b15c3294bc
UGYW <uma.w@outlook.com>
2019-06-19 15:05:09 +0200

    Added index functions.

--- AnalysisModules/Indices/index.py
+++ AnalysisModules/Indices/index.py
@@ -8,29 +8,51 @@ from AnalysisModules.Indices.index_7 import get_index_7
 from AnalysisModules.Indices.index_8 import get_index_8
 
 class Index:
-    def __init__(self, index_no, x_absorbance):
+    def __init__(self, index_no, x_absorbance, mask):
         self.index_no = index_no
         self.x_absorbance = x_absorbance
         assert index_no in [1, 2, 3, 4, 5, 6, 7, 8]
+
+        self.mask = mask
+
+        self.index = None
+        self.index_masked = None
+
         self._calc_index()
 
     def get_index(self):
         return self.index
 
+    def get_index_masked(self):
+        return self.index_masked
+
     def _calc_index(self):
+        # Get raw index
+        self._calc_raw_index()
+        self._calc_index_gradient()
+
+    def _calc_raw_index(self):
         if self.index_no == 1:
             self.index = get_index_1(self.x_absorbance)
         elif self.index_no == 2:
             self.index = get_index_2(self.x_absorbance)
-        elif self.index_no == 2:
+        elif self.index_no == 3:
             self.index = get_index_3(self.x_absorbance)
-        elif self.index_no == 2:
+        elif self.index_no == 4:
             self.index = get_index_4(self.x_absorbance)
-        elif self.index_no == 2:
+        elif self.index_no == 5:
             self.index = get_index_5(self.x_absorbance)
-        elif self.index_no == 2:
+        elif self.index_no == 6:
             self.index = get_index_6(self.x_absorbance)
-        elif self.index_no == 2:
+        elif self.index_no == 7:
             self.index = get_index_7(self.x_absorbance)
-        elif self.index_no == 2:
+        elif self.index_no == 8:
             self.index = get_index_8(self.x_absorbance)
+
+    # TODO: get index gradient
+    def _calc_index_gradient(self):
+        # Todo: I'm not sure why this isn't working
+        # index_gradient = np.gradient(index)
+        # index_gradient = index_gradient[:, :, wavelength[0], wavelength[1]]
+        # index = (index_gradient)
+        pass
--- AnalysisModules/utility.py
+++ AnalysisModules/utility.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-def read(file_to_read):
-    data = np.fromfile(file_to_read, dtype='>f')  # returns 1D array and reads file in big-endian binary format
-    data_cube = data[3:].reshape(640, 480, 100)  # reshape to data cube and ignore first 3 values which are wrong
-    return data_cube
-
+def make_mask():
+    # TODO: Complete once interactive graph module is completed
+    mask = None
+    return mask
