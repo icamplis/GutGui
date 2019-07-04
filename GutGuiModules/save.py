d0ae0ba02b5277c922310b33aab8ec8378bf773c
UGYW <uma.w@outlook.com>
2019-07-04 11:16:12 +0200

    Updates in save

--- GutGuiModules/save.py
+++ GutGuiModules/save.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+import logging
 
 class Save:
     def __init__(self, save_frame, listener):
@@ -36,6 +37,13 @@ class Save:
             ABSORPTION_SPEC_IMAGE_WO_SCALE: False,
             ABSORPTION_SPEC_EXCEL: False
         }
+
+        # The current data cube whose data is being saved.
+        # Used to access results from listener
+        self.current_result_key = ""
+        self.current_result = None  # for readability
+        # The current path to save to
+        # i.e the current result key's dirname
         self.current_output_path = ""
 
         self._init_widgets()
@@ -61,6 +69,8 @@ class Save:
             self._save_to_path(path)
 
     def _save_to_path(self, path):
+        self.current_result_key = path
+        self.current_result = self.listener.get_result(self.current_result_key)
         self.current_output_path = os.path.dirname(path)
 
         if self.saves[STO2_DATA]:
@@ -92,133 +102,123 @@ class Save:
             self.__save_absorption_spec()
 
     def __save_sto2_data_and_image(self):
-        # if save with scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save wo scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        pass
+        if self.saves[STO2_DATA]:
+            if self.saves[WHOLE_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_sto2(), "STO2_DATA")
+                self.__save_image(self.current_result.get_sto2(), "STO2_WHOLE_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
+            if self.saves[MASKED_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_sto2_masked(), "STO2_DATA_MASKED")
+                self.__save_image(self.current_result.get_sto2_masked(), "STO2_MASKED_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
 
     def __save_nir_data_and_image(self):
-        # if save with scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save wo scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        pass
+        if self.saves[NIR_DATA]:
+            if self.saves[WHOLE_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_nir(), "NIR_DATA")
+                self.__save_image(self.current_result.get_nir(), "NIR_WHOLE_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
+            if self.saves[MASKED_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_nir_masked(), "NIR_DATA_MASKED")
+                self.__save_image(self.current_result.get_nir_masked(), "NIR_MASKED_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
 
     def __save_twi_data_and_image(self):
-        # if save with scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save wo scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        pass
+        if self.saves[TWI_DATA]:
+            if self.saves[WHOLE_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_twi(), "TWI_DATA")
+                self.__save_image(self.current_result.get_twi(), "TWI_WHOLE_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
+            if self.saves[MASKED_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_twi_masked(), "TWI_DATA_MASKED")
+                self.__save_image(self.current_result.get_twi_masked(), "TWI_MASKED_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
 
     def __save_thi_data_and_image(self):
-        # if save with scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save wo scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        pass
+        if self.saves[THI_DATA]:
+            if self.saves[WHOLE_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_thi(), "THI_DATA")
+                self.__save_image(self.current_result.get_thi(), "THI_WHOLE_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
+            if self.saves[MASKED_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_thi_masked(), "THI_DATA_MASKED")
+                self.__save_image(self.current_result.get_thi_masked(), "THI_MASKED_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
 
     def __save_wl_data_and_image(self):
-        # if save with scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save wo scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
+        # todo: There is no WL data or image currently
+        if self.saves[WL_DATA]:
+            if self.saves[WHOLE_IMAGE_SAVE]:
+                pass
+                # self.__save_data(self.current_result.get_wl(), "WL_DATA")
+                # self.__save_image(self.current_result.get_wl(), "WL_WHOLE_IMAGE",
+                #                   self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
+            if self.saves[MASKED_IMAGE_SAVE]:
+                pass
+                # self.__save_data(self.current_result.get_wl_masked(), "WL_DATA_MASKED")
+                # self.__save_image(self.current_result.get_wl_masked(), "WL_MASKED_IMAGE",
+                #                   self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
         pass
 
     def __save_idx_data_and_image(self):
-        # if save with scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save wo scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        pass
+        if self.saves[IDX_DATA]:
+            if self.saves[WHOLE_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_index(), "INDEX_DATA")
+                self.__save_image(self.current_result.get_index(), "INDEX_WHOLE_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
+            if self.saves[MASKED_IMAGE_SAVE]:
+                self.__save_data(self.current_result.get_index_masked(), "INDEX_DATA_MASKED")
+                self.__save_image(self.current_result.get_index_masked(), "INDEX_MASKED_IMAGE",
+                                  self.saves[REC_IMAGE], self.saves[REC_IMAGE_WO_SCALE])
 
     def __save_histogram(self):
-        # if save with scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save wo scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save to excel:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        pass
+        if self.saves[WHOLE_IMAGE_SAVE]:
+            pass
+        if self.saves[MASKED_IMAGE_SAVE]:
+            pass
+        if self.saves[HISTOGRAM_EXCEL]:
+            pass
 
     def __save_absorption_spec(self):
-        # if save with scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save wo scale:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        # if save to excel:
-        #     if save whole image
-        #       todo
-        #     if save masked image
-        #       todo
-        pass
+        if self.saves[WHOLE_IMAGE_SAVE]:
+            pass
+        if self.saves[MASKED_IMAGE_SAVE]:
+            pass
+        if self.saves[ABSORPTION_SPEC_EXCEL]:
+            pass
 
     def __save_data(self, data, title, format=".csv"):
-        np.savetxt(title + format, data, delimiter=",")
+        print("save csv data placeholder")
+        output_path = self.current_output_path + "/" + title + format
+        logging.debug("SAVING DATA TO " + output_path)
+        # np.savetxt(self.current_output_path + "/" + title + format, data, delimiter=",")
 
-    def __save_image(self, data, title, format=".png", vmin=0, vmax=1):
-        plt.title(title)
-        plt.imsave(title + format, data[:, :], cmap='jet', vmin=vmin, vmax=vmax)
-        plt.clf()
+    def __save_image(self, data, title, is_image_with_scale, is_image_wo_scale,
+                     format=".png", vmin=0, vmax=1):
+        if is_image_with_scale:
+            self.__save_image_with_scale(data, title, format, vmin, vmax)
+        if is_image_wo_scale:
+            self.__save_image_wo_scale((data, title, format, vmin, vmax))
+
+    def __save_image_with_scale(self, data, title, format=".png", vmin=0, vmax=1):
+        print("save image with scale placeholder")
+        output_path = self.current_output_path + "/" + title + format
+        logging.debug("SAVING IMAGE TO " + output_path)
+        # plt.title(title)
+        # plt.imsave(output_path, data[:, :], cmap='jet', vmin=vmin, vmax=vmax)
+        # plt.clf()
 
     def __save_image_wo_scale(self, data, title, format=".png", vmin=0, vmax=1):
-        plt.axis('off')  # removes the axis
-        plt.imsave(title + format, data[:, :], cmap='jet', vmin=vmin, vmax=vmax)
-        plt.clf()
+        print("save image without scale placeholder")
+        output_path = self.current_output_path + "/" + title + format
+        logging.debug("SAVING IMAGE WO SCALE TO " + output_path)
+        # plt.axis('off')  # removes the axis
+        # plt.imsave(output_path, data[:, :], cmap='jet', vmin=vmin, vmax=vmax)
+        # plt.clf()
 
     def __save_excel(self, data, title, format=".xlsx"):
-        path = title + format
-        df = pd.DataFrame(data)
-        df.to_excel(path, index=False)
+        print("save excel")
+        output_path = self.current_output_path + "/" + title + format
+        logging.debug("SAVING EXCEL TO " + output_path)
+        # df = pd.DataFrame(data)
+        # df.to_excel(output_path, index=False)
