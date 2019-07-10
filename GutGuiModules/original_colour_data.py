4c84b91003ce01eff0208cbf97071fc50ecd05f2
isabellacamplisson <icamplis@caltech.edu>
2019-07-10 16:24:41 +0200

    colour data

--- GutGuiModules/original_colour_data.py
+++ GutGuiModules/original_colour_data.py
@@ -35,4 +35,14 @@ class OGColourData:
 
     def _build_data(self):
         # mean
-        pass
+        self.mean_text = make_text(self.root, content="Mean = " + str(self.mean_value), bg=tkcolour_from_rgb(PASTEL_PINK_RGB), column=0, row=1, width=12, columnspan=1, padx=0, state=NORMAL)
+        # standard deviation
+        self.sd_text = make_text(self.root, content="SD = " + str(self.sd_value), bg=tkcolour_from_rgb(PASTEL_PINK_RGB), column=0, row=2, width=10, columnspan=1, padx=0, state=NORMAL)
+        # median
+        self.median_text = make_text(self.root, content="Median = " + str(self.median_value), bg=tkcolour_from_rgb(PASTEL_PINK_RGB), column=0, row=3, width=14, columnspan=1, padx=0, state=NORMAL)
+        # IQR
+        self.iqr_text = make_text(self.root, content="IQR = " + str(self.iqr_value), bg=tkcolour_from_rgb(PASTEL_PINK_RGB), column=0, row=4, width=20, columnspan=1, padx=0, state=NORMAL)
+        # min
+        self.min_text = make_text(self.root, content="Min = " + str(self.min_value), bg=tkcolour_from_rgb(PASTEL_PINK_RGB), column=0, row=5, width=11, columnspan=1, padx=0, state=NORMAL)
+        # max
+        self.max_text = make_text(self.root, content="Max = " + str(self.max_value), bg=tkcolour_from_rgb(PASTEL_PINK_RGB), column=0, row=6, width=11, columnspan=1, padx=0, state=NORMAL)
